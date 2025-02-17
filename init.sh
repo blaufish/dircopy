9e8ffe2b665f55f873d21b947322cb324742c178
blaufish <blaufish@users.noreply.github.com>
2025-02-17 14:50:00 +0100

    Hello World

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+depends_on() {
+	if which -- "$1" 2>&1 > /dev/null
+	then
+		echo -n ""
+	else
+		>&2 echo "please install $1, maybe sudo apt install $1"
+		exit 2
+	fi
+}
+
+if [[ ! -d .git ]]
+then
+	git init .
+fi
+
+depends_on rustc 
+depends_on cargo
+
+if [[ ! -d hello_world ]]
+then
+	cargo init hello_world
+fi
