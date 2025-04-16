24695ae80d66862850e41dad287f62f0cd139daa
blaufish <blaufish@users.noreply.github.com>
2025-04-16 09:45:59 +0200

    Add subdirs to tests

--- dircopy/build-test.sh
+++ dircopy/build-test.sh
@@ -9,10 +9,15 @@ then
 fi
 
 mkdir -p -- "$DIR/src/"
-touch -- "$DIR/src/0"
-for size in 1 1022 1023 1024 1025 1026 1048573 1048574 1048576 1048577 1048578
+mkdir -p -- "$DIR/src/subdir_a/subdir_b"
+mkdir -p -- "$DIR/src/subdir_c"
+for subdir in "$DIR/src/" "$DIR/src/subdir_a/subdir_b" "$DIR/src/subdir_c"
 do
-	dd if=/dev/urandom of="$DIR/src/$size" count=1 bs="$size"
+	touch -- "$subdir/0"
+	for size in 1 1022 1023 1024 1025 1026 1048573 1048574 1048576 1048577 1048578
+	do
+		dd if=/dev/urandom of="$subdir/$size" count=1 bs="$size"
+	done
 done
 
 mkdir -p -- "$DIR/dst/"
